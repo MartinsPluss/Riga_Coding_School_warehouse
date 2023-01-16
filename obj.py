9bfaf48e7ae924bdeeeacc5778a3cdcfcc334939
MartinsPluss <120741332+MartinsPluss@users.noreply.github.com>
2023-01-16 10:39:09 +0200

    pievienoti komentari

--- obj.py
+++ obj.py
@@ -1,16 +1,26 @@
+# Šī ir CRUD programma grāmatu noliktavai ar Mysql datubāzi
+
+import re
 import mysql.connector as mysql
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
+
+# Definē objektu
 
 
 class Db:
 
     def __init__(self):
-        self.root = Tk()
-        self.root.geometry("1000x500+700+500")  # Martins
-        self.root.title("Gramatu noliktava")  # Martins
-        frm = ttk.Frame(self.root, padding=10)
-        frm.grid()
+        self.app = Tk()
+        w = self.app.winfo_screenwidth()
+        h = self.app.winfo_screenheight()
+        self.app.columnconfigure(4, weight=1)
+        self.app.rowconfigure(3, weight=1)
+        self.app.title("Gramatu noliktava")  # Martins
+        self.app.geometry(f"{w}x{h}")  # Martins
+
+        # Datubāzes savienojums
 
         self.conn = mysql.connect(
             host="localhost",
@@ -20,50 +30,69 @@ class Db:
         )
         self.cursor = self.conn.cursor()
 
-        self.column1_input = Entry(self.root)
-        self.column2_input = Entry(self.root)
-        self.column3_input = Entry(self.root)
+        # Ievades lauki
 
-        self.column1_label = Label(self.root, text="Nosaukums:")  # Martins
-        self.column2_label = Label(self.root, text="Skaits:")
-        self.column3_label = Label(self.root, text="Apraksts:")
+        self.column1_input = Entry(self.app)
+        self.column2_input = Entry(self.app)
+        self.column3_input = Entry(self.app)
 
-        self.column1_label.grid(row=0, column=0)
-        self.column1_input.grid(row=0, column=1)
-        self.column2_label.grid(row=1, column=0)
-        self.column2_input.grid(row=1, column=1)
-        self.column3_label.grid(row=2, column=0)
-        self.column3_input.grid(row=2, column=1)
+        self.column1_label = Label(self.app, text="Nosaukums:")  # Martins
+        self.column2_label = Label(self.app, text="Skaits:")
+        self.column3_label = Label(self.app, text="Apraksts:")
+
+        self.column1_label.grid(row=0, column=0, padx=10, pady=5)
+        self.column1_input.grid(row=0, column=1, padx=10, pady=3)
+        self.column2_label.grid(row=1, column=0, padx=10, pady=5)
+        self.column2_input.grid(row=1, column=1, padx=10, pady=3)
+        self.column3_label.grid(row=2, column=0, padx=10, pady=3)
+        self.column3_input.grid(row=2, column=1, padx=10, pady=3)
+
+        # Pogas
 
         self.insert_button = ttk.Button(
-            self.root, text="Ievietot", command=self.insert)
-        self.insert_button.grid(row=3, column=4)  # Martins
+            self.app, text="Ievietot", command=self.insert)
+        self.insert_button.grid(row=0, column=3, padx=10)   # Martins
 
         self.update_button = ttk.Button(
-            self.root, text="Labot", command=self.update_database)
-        self.update_button.grid(row=3, column=5)
+            self.app, text="Labot", command=self.update_database)
+        self.update_button.grid(row=1, column=3)
 
         self.view_button = ttk.Button(
-            self.root, text="Apskatīt", command=self.view)
-        self.view_button.grid(row=3, column=6)
+            self.app, text="Apskatīt", command=self.view)
+        self.view_button.grid(row=2, column=3)
 
         self.delete_button = ttk.Button(
-            self.root, text="Izdzēst", command=self.delete_item)
-        self.delete_button.grid(row=3, column=7)
+            self.app, text="Izdzēst", command=self.delete_item)
+        self.delete_button.grid(row=3, column=3, sticky=N)
 
-        def close():  # Martins
-            self.root.destroy()
+        self.quit_button = ttk.Button(
+            self.app, text="Beigt darbu", command=self.app.destroy)
+        self.quit_button.grid(row=6, column=6, padx=20, pady=20)
 
-        # Create a Button to call close()
-        quit_button = ttk.Button(self.root, text="Beigt darbu", command=close)
-        quit_button.grid(row=6, column=7)
-        self.root.mainloop()
+        self.app.mainloop()
+
+        # Teksta ievades validācija
+
+    def is_valid_title(self, input_string):
+        return bool(re.match(r'^[A-Za-z0-9\sĀČĢĶĻŅŠŪŽāčģīķļņšūž]+$', input_string))
+
+    def is_number(self, input_string):
+        return bool(re.match(r'^\d+$', input_string))
+
+        # Ievietošanas funkcija
 
     def insert(self):
 
-        # Get the values from the input fields
-        title = self.column1_input.get()
-        number = self.column2_input.get()
+        if self.is_valid_title(self.column1_input.get().strip()):
+            title = self.column1_input.get().strip()
+        else:
+            messagebox.showerror("Kļūda", "Nosaukums nav derīgs")
+
+        if self.is_number(self.column2_input.get().strip()):
+            number = int(self.column2_input.get().strip())
+        else:
+            messagebox.showerror("Kļūda", "Skaits nav derīgs")
+
         description = self.column3_input.get()
 
         insert = "INSERT INTO warehouse (title, number, description) VALUES(%s,%s,%s)"
@@ -72,37 +101,69 @@ class Db:
         self.conn.commit()
         print("New records added")
 
+        self.view()
+
+        # Labošanas funkcija
+
     def update_database(self):
 
-        title = self.column1_input.get()
-        number = self.column2_input.get()
+        title = self.column1_input.get().strip()
+
+        if self.is_number(self.column2_input.get().strip()):
+            number = int(self.column2_input.get().strip())
+        else:
+            messagebox.showerror("Kļūda", "Skaits nav derīgs")
+
         description = self.column3_input.get()
 
         update = "UPDATE warehouse SET number = %s WHERE title = %s"
         values = (number, title)
         self.cursor.execute(update, values)
 
-        # self.column2_input.cursor.execute("UPDATE warehouse SET number = %s, description=%s WHERE title=%s", (number, description, title))
         print("Updated")
         self.conn.commit()
 
-        # # Update the button text to indicate that the update was successful
-        # update_button.config(text="Update Successful!")
+        self.view()
+
+        # apskatīšanas funkcija
 
     def view(self):
-
         view = "SELECT title, number, description FROM warehouse"  # Martins
         self.cursor.execute(view)
         results = self.cursor.fetchall()
 
-        results_text = Text(self.root)
+        # Create separate Label and Text widgets for each field
+        title_label = Label(self.app, text="Nosaukums")
+        title_label.grid(row=4, column=4)
+
+        results_text_title = Text(self.app, width=70)
+        results_text_title.grid(row=5, column=4, padx=0, pady=0)
+
+        number_label = Label(self.app, text="Skaits")
+        number_label.grid(row=4, column=5)
+
+        results_text_number = Text(self.app, width=50)
+        results_text_number.grid(row=5, column=5, padx=5, pady=5)
+
+        description_label = Label(self.app, text="Apraksts")
+        description_label.grid(row=4, column=6)
+
+        results_text_description = Text(self.app, width=130)
+        results_text_description.grid(row=5, column=6, padx=0, pady=0)
 
         for result in results:
-            results_text.insert(END, result)
-            results_text.insert(END, "\n")
+            results_text_title.insert(END, result[0])
+            results_text_title.insert(END, "\n")
+            results_text_number.insert(END, result[1])
+            results_text_number.insert(END, "\n")
+            results_text_description.insert(END, result[2])
+            results_text_description.insert(END, "\n")
 
-        # Position the Text widget on the window # Martins
-        results_text.grid(row=4, columnspan=2)
+        self.app.columnconfigure(4, weight=1, minsize=50)
+        self.app.columnconfigure(5, weight=1, minsize=50)
+        self.app.columnconfigure(6, weight=1, minsize=50)
+
+        # Dzēšanas funkcija
 
     def delete_item(self):
 
@@ -112,6 +173,8 @@ class Db:
         self.cursor.execute(delete, (title,))
         self.conn.commit()
         print("Deleted")  # Martins
+
+        self.view()
 
 
 if __name__ == '__main__':
